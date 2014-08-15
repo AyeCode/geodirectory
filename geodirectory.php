a8182c65e87cca67864b9fbe16b2f236596d9695
paoltaia <paoltaia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-15 18:20:52 +0000

    tagging version 1.2.0
    git-svn-id: https://plugins.svn.wordpress.org/geodirectory/trunk@966503 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- geodirectory.php
+++ geodirectory.php
@@ -3,7 +3,7 @@
 Plugin Name: GeoDirectory
 Plugin URI: http://wpgeodirectory.com/
 Description: GeoDirectory plugin for wordpress.
-Version: 1.1.9
+Version: 1.2.0
 Author: GeoDirectory
 Author URI: http://wpgeodirectory.com
 Requires at least: 3.1
@@ -11,7 +11,7 @@ Tested up to: 3.9.1
 */
 
 
-define("GEODIRECTORY_VERSION", "1.1.9");
+define("GEODIRECTORY_VERSION", "1.2.0");
 
 if (!session_id()) session_start();
 
