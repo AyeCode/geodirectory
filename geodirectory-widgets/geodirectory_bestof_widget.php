015c7b9033127272c5b7cb5b23dddda44cad4e56
Giri <giridharanbtech@gmail.com>
2015-02-25 13:11:39 +0530

    bestof widget icon alt

--- geodirectory-widgets/geodirectory_bestof_widget.php
+++ geodirectory-widgets/geodirectory_bestof_widget.php
@@ -136,7 +136,7 @@ class geodir_bestof_widget extends WP_Widget {
                     }
                     $term_icon_url = $term_icon[$cat->term_id];
                     $nav_html .= '<a data-termid="' . $cat->term_id . '" href="' . get_term_link($cat, $cat->taxonomy) . '">';
-                    $nav_html .= '<img class="bestof-cat-icon" src="' . $term_icon_url . '"/>';
+                    $nav_html .= '<img alt="'.$cat->name.' icon" class="bestof-cat-icon" src="' . $term_icon_url . '"/>';
                     $nav_html .= '<span>';
                     $nav_html .= ucwords($cat->name);
                     $nav_html .= '<small>';
