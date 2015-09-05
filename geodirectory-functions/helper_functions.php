0b9cf038e35e7c96d2f9fb8c7c959dd54a3bde4f
NomadDevs <info@wpgeodirectory.com>
2015-09-05 18:22:27 +0100

    In some cases with https when saving a listing it can change image url - FIXED

--- geodirectory-functions/helper_functions.php
+++ geodirectory-functions/helper_functions.php
@@ -139,3 +139,26 @@ function geodir_login_url($args=array()){
     return $login_url;
 }
 
+function geodir_info_url($args=array()){
+    $gd_page_id = get_option('geodir_info_page');
+
+    if (function_exists('icl_object_id')) {
+        $gd_page_id =  icl_object_id($gd_page_id, 'page', true);
+    }
+
+    if($gd_page_id){
+        $post = get_post($gd_page_id);
+        $slug = $post->post_name;
+        //$login_url = get_permalink($gd_page_id );// get_permalink can only be user after theme-Setup hook, any earlier and it errors
+        $info_url = home_url()."/$slug/";
+    }else{
+        $info_url = home_url()."/";
+    }
+
+    if($args){
+        $info_url = add_query_arg($args,$info_url );
+    }
+
+    return $info_url;
+}
+
