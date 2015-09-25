b1108b23a03fc8bb6b1f5c11fa5fe775ffb1a868
Kiran Prajapati <kprajapati@thakkertech.com>
2015-09-25 19:09:54 +0530

    New widget "CPT Categories" added to list categories - ADDED

--- geodirectory_widgets.php
+++ geodirectory_widgets.php
@@ -1083,6 +1083,13 @@ if (!function_exists('register_geodir_widgets')) {
          * @package GeoDirectory
          */
         include_once('geodirectory-widgets/geodirectory_bestof_widget.php');
+		/**
+         * Contains all functions for cpt categories widget.
+         *
+         * @since 1.5.3
+         * @package GeoDirectory
+         */
+        include_once('geodirectory-widgets/geodirectory_cpt_categories_widget.php');
     }
 
 }
