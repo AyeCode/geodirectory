8594c14432341da816a9004a7e9c91379833dfc5
Giri Dhar <giridharanbtech@gmail.com>
2015-11-28 22:08:43 +0530

    some more filters added

--- geodirectory-templates/widget-listing-listview.php
+++ geodirectory-templates/widget-listing-listview.php
@@ -36,7 +36,7 @@ if (isset($_SESSION['gd_listing_view']) && $_SESSION['gd_listing_view'] != '' &&
     }
 }
 ?>
-    <ul class="geodir_category_list_view clearfix">
+    <ul class="geodir_category_list_view clearfix <?php echo apply_filters('geodir_listing_listview_ul_extra_class', '', 'widget'); ?>">
         <?php
         if (!empty($widget_listings)) {
 
