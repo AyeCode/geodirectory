ddf0ef196349a977d1907c73e96e2e0dd3ab9f8d
NomadDevs <info@wpgeodirectory.com>
2016-01-06 14:15:25 +0000

    Font awesome updated to 4.5.0 - CHANGED

--- geodirectory-admin/admin_functions.php
+++ geodirectory-admin/admin_functions.php
@@ -86,7 +86,7 @@ if (!function_exists('geodir_admin_styles_req')) {
     function geodir_admin_styles_req()
     {
 
-        wp_register_style('geodirectory-font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', array(), GEODIRECTORY_VERSION);
+        wp_register_style('geodirectory-font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css', array(), GEODIRECTORY_VERSION);
         wp_enqueue_style('geodirectory-font-awesome');
     }
 }
