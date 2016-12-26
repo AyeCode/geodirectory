0f5aaec47ad4eeb7bbed7726eab0ab689b7b2092
Kiran Prajapati <kprajapati@thakkertech.com>
2016-12-26 10:15:14 +0530

    Breadcrumb formatting issue with the neighbourhood name - FIXED

--- geodirectory-functions/general_functions.php
+++ geodirectory-functions/general_functions.php
@@ -1234,6 +1234,7 @@ function geodir_wpml_post_type_archive_link($link, $post_type){
  *
  * @since   1.0.0
  * @since   1.5.7 Changes for the neighbourhood system improvement.
+ * @since   1.6.16 Fix: Breadcrumb formatting issue with the neighbourhood name.
  * @package GeoDirectory
  * @global object $wp_query   WordPress Query object.
  * @global object $post       The current post object.
@@ -1372,6 +1373,7 @@ function geodir_breadcrumb() {
 						$location_term_actual_country = '';
 						$location_term_actual_region  = '';
 						$location_term_actual_city    = '';
+						$location_term_actual_neighbourhood = '';
 						if ( $geodir_get_locations ) {
 							if ( $key == 'gd_country' ) {
 								$location_term_actual_country = get_actual_location_name( 'country', $location_term, true );
@@ -1379,6 +1381,8 @@ function geodir_breadcrumb() {
 								$location_term_actual_region = get_actual_location_name( 'region', $location_term, true );
 							} else if ( $key == 'gd_city' ) {
 								$location_term_actual_city = get_actual_location_name( 'city', $location_term, true );
+							} else if ( $key == 'gd_neighbourhood' ) {
+								$location_term_actual_neighbourhood = get_actual_location_name( 'neighbourhood', $location_term, true );
 							}
 						} else {
 							$location_info = geodir_get_location();
@@ -1401,7 +1405,7 @@ function geodir_breadcrumb() {
 						} else if ( $is_location_last && $key == 'gd_city' && empty( $location_terms['gd_neighbourhood'] ) ) {
 							$breadcrumb .= $location_term_actual_city != '' ? $separator . $location_term_actual_city : $separator . $gd_location_link_text;
 						} else if ( $is_location_last && $key == 'gd_neighbourhood' ) {
-							$breadcrumb .= $separator . $gd_location_link_text;
+							$breadcrumb .= $location_term_actual_neighbourhood != '' ? $separator . $location_term_actual_neighbourhood : $separator . $gd_location_link_text;
 						} else {
 							if ( get_option( 'permalink_structure' ) != '' ) {
 								$location_link .= $location_term . '/';
@@ -1415,6 +1419,8 @@ function geodir_breadcrumb() {
 								$gd_location_link_text = $location_term_actual_region;
 							} else if ( $key == 'gd_city' && $location_term_actual_city != '' ) {
 								$gd_location_link_text = $location_term_actual_city;
+							} else if ( $key == 'gd_neighbourhood' && $location_term_actual_neighbourhood != '' ) {
+								$gd_location_link_text = $location_term_actual_neighbourhood;
 							}
 
 							/*
