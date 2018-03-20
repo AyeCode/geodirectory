16d6b3eaf6c2c1d4c7f9dc732e818523ddad1679
stiofan <info@wpgeodirectory.com>
2018-03-20 19:15:01 +0000

    2.0.0.1-beta

--- geodirectory.php
+++ geodirectory.php
@@ -11,7 +11,7 @@
  * Plugin Name: GeoDirectory
  * Plugin URI: https://wpgeodirectory.com/
  * Description: GeoDirectory plugin for WordPress.
- * Version: 2.0.0.0-beta
+ * Version: 2.0.0.1-beta
  * Author: AyeCode Ltd
  * Author URI: https://wpgeodirectory.com
  * Text Domain: geodirectory
@@ -60,7 +60,7 @@ final class GeoDirectory {
      *
      * @var string
      */
-    public $version = '2.0.0.0-beta';
+    public $version = '2.0.0.1-beta';
     
     /**
      * GeoDirectory instance.
