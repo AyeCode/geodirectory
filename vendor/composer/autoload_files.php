1bc12cf5925c1158bc45e9a2ae397da55d64e2dd
stiofan <info@wpgeodirectory.com>
2018-12-05 16:46:48 +0000

    composer lib updates

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,6 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    '24583d3588ebda5228dd453cfaa070da' => $vendorDir . '/ayecode/wp-font-awesome-settings/wp-font-awesome-settings.php',
     '942e926b62933a5c0292cfd46ab28c95' => $vendorDir . '/ayecode/wp-country-database/wp-country-database.php',
+    '24583d3588ebda5228dd453cfaa070da' => $vendorDir . '/ayecode/wp-font-awesome-settings/wp-font-awesome-settings.php',
 );
