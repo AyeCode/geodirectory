21c1f82be98e699cef92ea15cc2ad5fe8499c9f2
Kiran Prajapati <kprajapati@thakkertech.com>
2019-01-02 18:16:51 +0530

    v1 to v2 conversion changes

--- includes/admin/class-geodir-admin-install.php
+++ includes/admin/class-geodir-admin-install.php
@@ -81,7 +81,12 @@ class GeoDir_Admin_Install {
 			GeoDir_Admin_Notices::add_notice( 'update' );
 		}
 		if ( ! empty( $_GET['force_update_geodirectory'] ) ) {
-			do_action( 'wp_geodir_updater_cron' );
+			$blog_id = get_current_blog_id();
+
+			// Used to fire an action added in WP_Background_Process::_construct() that calls WP_Background_Process::handle_cron_healthcheck().
+			// This method will make sure the database updates are executed even if cron is disabled. Nothing will happen if the updates are already running.
+			do_action( 'wp_' . $blog_id . '_geodir_updater_cron' );
+
 			wp_safe_redirect( admin_url( 'admin.php?page=gd-settings' ) );
 			exit;
 		}
