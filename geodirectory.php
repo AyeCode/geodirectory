1731b41c6f32f73c59b66ff9f4f545c272062691
stiofan <info@wpgeodirectory.com>
2019-03-04 15:43:04 +0000

    2.0.0.46

--- geodirectory.php
+++ geodirectory.php
@@ -11,13 +11,13 @@
  * Plugin Name: GeoDirectory
  * Plugin URI: https://wpgeodirectory.com/
  * Description: GeoDirectory plugin for WordPress.
- * Version: 2.0.0.45
+ * Version: 2.0.0.46
  * Author: AyeCode Ltd
  * Author URI: https://wpgeodirectory.com
  * Text Domain: geodirectory
  * Domain Path: /languages
  * Requires at least: 4.5
- * Tested up to: 5.0
+ * Tested up to: 5.1
  * Update URL: https://github.com/AyeCode/geodirectory/
  */
 
@@ -60,7 +60,7 @@ final class GeoDirectory {
      *
      * @var string
      */
-    public $version = '2.0.0.45';
+    public $version = '2.0.0.46';
     
     /**
      * GeoDirectory instance.
