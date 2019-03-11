cb88445ee8561360453f99d1e9ff93c69321cbd0
Naveen giri <1naveengiri@gmail.com>
2019-03-11 18:40:01 +0530

    No listings found message should contain add listing link #408

--- templates/loop/no-listings-found.php
+++ templates/loop/no-listings-found.php
@@ -6,6 +6,15 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
-
+$show_add_listing = GeoDir_User::show_add_listings('array');
 ?>
-<p class="geodir-info"><?php _e( 'No listings were found matching your selection.', 'geodirectory' ); ?></p>
+<p class="geodir-info">
+    <?php
+
+    if( isset( $show_add_listing[0]['url'] ) && !empty( $show_add_listing[0]['url'] )){
+        printf( __( "No listings were found matching your selection, <a href='%s'>Add Listing</a>.", 'geodirectory' ), $show_add_listing[0]['url'] );
+    } else{
+	    _e( "No listings were found matching your selection.", 'geodirectory' );
+    }
+    ?>
+</p>
