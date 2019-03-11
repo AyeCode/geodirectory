e63a3334dee773a4c9fcf2351a68e52c56b78e8e
Naveen giri <1naveengiri@gmail.com>
2019-03-11 20:13:51 +0530

    Update function defination

--- templates/loop/no-listings-found.php
+++ templates/loop/no-listings-found.php
@@ -6,13 +6,13 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
-$show_add_listing = GeoDir_User::show_add_listings('array');
+$show_add_listing = GeoDir_User::show_add_listings('gd_place');
 ?>
 <p class="geodir-info">
     <?php
 
-    if( isset( $show_add_listing[0]['url'] ) && !empty( $show_add_listing[0]['url'] )){
-        printf( __( "No listings were found matching your selection, <a href='%s'>Add Listing</a>.", 'geodirectory' ), $show_add_listing[0]['url'] );
+    if( isset( $show_add_listing['url'] ) && !empty( $show_add_listing['url'] )){
+        printf( __( "No listings were found matching your selection. Something missing? Why not <a href='%s'>add a listing?</a>.", 'geodirectory' ), $show_add_listing['url'] );
     } else{
 	    _e( "No listings were found matching your selection.", 'geodirectory' );
     }
