3aaa286647b95e1f658cc0b025c23bf42d3b3605
Kiran Prajapati <officeofkiran@gmail.com>
2021-05-26 19:44:09 +0530

    Category image not added in Yoast SEO meta - FIXED

--- vendor/ayecode/wp-ayecode-ui/includes/ayecode-ui-settings.php
+++ vendor/ayecode/wp-ayecode-ui/includes/ayecode-ui-settings.php
@@ -983,6 +983,37 @@ if ( ! class_exists( 'AyeCode_UI_Settings' ) ) {
 					aui_init();
 				});
 
+				/* Fix modal background scroll on iOS mobile device */
+				jQuery(function($) {
+					var ua = navigator.userAgent.toLowerCase();
+					var isiOS = ua.match(/(iphone|ipod|ipad)/);
+					if (isiOS) {
+						var pS = 0;
+						$(document).on('show.bs.modal', function() {
+							pS = window.scrollY;
+							$('.modal-open').css({
+								marginTop: -pS,
+								overflow: 'hidden',
+								left: 0,
+								right: 0,
+								top: 0,
+								bottom: 0,
+								position: 'fixed',
+							});
+						}).on('hidden.bs.modal', function() {
+							$('.modal-open').css({
+								marginTop: 0,
+								overflow: 'visible',
+								left: 'auto',
+								right: 'auto',
+								top: 'auto',
+								bottom: 'auto',
+								position: 'inherit',
+							});
+							window.scrollTo(0, pS);
+						});
+					}
+				});
 			</script>
 			<?php
 			$output = ob_get_clean();
