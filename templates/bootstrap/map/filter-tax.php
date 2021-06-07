c26c4c25f91c11210247ca223f225ea957741984
Kiran Prajapati <officeofkiran@gmail.com>
2021-06-07 11:17:45 +0530

    Map CPT filter only visible when search filter is active - FIXED

--- templates/bootstrap/map/filter-tax.php
+++ templates/bootstrap/map/filter-tax.php
@@ -1,16 +1,26 @@
 <?php
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
 /**
- * Variables.
+ * Display Map Filters
+ *
+ * This template can be overridden by copying it to yourtheme/geodirectory/bootstrap/map/filter-tax.php.
+ *
+ * HOWEVER, on occasion GeoDirectory will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see        https://docs.wpgeodirectory.com/article/346-customizing-templates/
+ * @package    GeoDirectory
+ * @version    2.1.0.16
  *
  * @var array $map_options The map settings options.
  * @var string $map_canvas The map canvas string.
  * @var string $cat_filter_class The category filter class.
  * @var array $map_post_types The custom post types for the map.
  */
+
+defined( 'ABSPATH' ) || exit;
 ?>
 <!-- START cat_filter/search_filter -->
 <div class="geodir-map-cat-filter-wrap position-absolute row m-0 text-light px-2 overflow-hidden z-index-1" style="max-height: 100%;right:0;bottom:0;background:#000000a1;<?php echo ( geodir_lazy_load_map() == 'click' ? 'display:none;' : '' ); ?>">
@@ -39,25 +49,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 				}
 			}
 
-			echo aui()->input(
-				array(
-					'id'                => "{$map_canvas}_search_string",
-					'name'              => "search",
-					'type'              => "search",
-					'placeholder'       => esc_attr__( 'Search by name', 'geodirectory' ),
-					'title'       => esc_attr__( 'hit enter to search', 'geodirectory' ),
-					'class'             => 'form-control-sm',
-					'no_wrap'           => true,
-					'extra_attributes'  => array(
-						'autocomplete' => 'off',
-						'data-toggle' => 'tooltip',
-						'data-placement' => 'left',
-						'onkeydown'       => "if(event.keyCode == 13){build_map_ajax_search_param('$map_canvas', false);}",
-						'style'         => "box-sizing: border-box;",
-						'aria-label' => esc_attr__( 'Search by name', 'geodirectory' )
-					),
-				)
-			);
+			if ( ! empty( $map_options['search_filter'] ) ) {
+				echo aui()->input(
+					array(
+						'id'                => "{$map_canvas}_search_string",
+						'name'              => "search",
+						'type'              => "search",
+						'placeholder'       => esc_attr__( 'Search by name', 'geodirectory' ),
+						'title'       => esc_attr__( 'hit enter to search', 'geodirectory' ),
+						'class'             => 'form-control-sm',
+						'no_wrap'           => true,
+						'extra_attributes'  => array(
+							'autocomplete' => 'off',
+							'data-toggle' => 'tooltip',
+							'data-placement' => 'left',
+							'onkeydown'       => "if(event.keyCode == 13){build_map_ajax_search_param('$map_canvas', false);}",
+							'style'         => "box-sizing: border-box;",
+							'aria-label' => esc_attr__( 'Search by name', 'geodirectory' )
+						),
+					)
+				);
+			}
 			?>
 
 			<?php if ( ! empty( $map_options['cat_filter'] ) ) { ?>
