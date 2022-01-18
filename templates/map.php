97a843d2a6e42fc8f60398bdcf630651c4d03b2a
Kiran Prajapati <officeofkiran@gmail.com>
2022-01-18 19:04:53 +0530

    Google map don't populates region for some addresses within Greece - FIXED

--- templates/map.php
+++ templates/map.php
@@ -324,11 +324,13 @@ $icon_size = GeoDir_Maps::get_marker_size($marker_icon, array('w' => 20, 'h' => 
                 }
             }
 
-            // fix some countries without regions, Isle of Man, Singapore
-            if(getCountryISO=='IM'){
+            /* Fix some countries without regions, Isle of Man, Singapore, Greece. */
+            if (getCountryISO=='IM'){
                 getState = "Isle of Man";
             }else if(getCountryISO=='SG'){
                 getState = "Singapore";
+            } else if(getCountryISO == 'GR' && !getState && administrative_area_level_3.long_name) {
+                getState = administrative_area_level_3.long_name;
             }
 
             /* Fix region name for ‎Belgium */
