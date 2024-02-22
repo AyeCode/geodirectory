4f8760601783bdf500fd287db3fcfd2e2106c3b4
Kiran Prajapati <officeofkiran@gmail.com>
2024-02-22 18:37:39 +0530

    2.3.41

--- geodirectory.php
+++ geodirectory.php
@@ -11,7 +11,7 @@
  * Plugin Name: GeoDirectory
  * Plugin URI: https://wpgeodirectory.com/
  * Description: GeoDirectory - Business Directory Plugin for WordPress.
- * Version: 2.3.40
+ * Version: 2.3.41
  * Author: AyeCode - WordPress Business Directory Plugins
  * Author URI: https://wpgeodirectory.com
  * Text Domain: geodirectory
@@ -34,7 +34,7 @@ final class GeoDirectory {
 	 *
 	 * @var string
 	 */
-	public $version = '2.3.40';
+	public $version = '2.3.41';
 
 	/**
 	 * GeoDirectory instance.
