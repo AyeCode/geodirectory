d9989fd1014c4598685e714f540b37f580ca17ed
Kiran Prajapati <officeofkiran@gmail.com>
2025-01-09 22:15:46 +0530

    2.8.96

--- includes/libraries/geodir-fast-ajax.php
+++ includes/libraries/geodir-fast-ajax.php
@@ -9,8 +9,8 @@
  * License: GPL-2.0+
  * License: GNU General Public License v3 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * Requires at least: 4.5
- * Tested up to: 6.0
+ * Requires at least: 5.0
+ * Tested up to: 6.7
  */
 
 // Exit if accessed directly
