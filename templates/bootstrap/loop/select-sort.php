7bdc20529a8aa6d16b701c7a7db55dfa3a9cef1b
Kiran Prajapati <officeofkiran@gmail.com>
2025-06-11 16:13:42 +0530

    select sort template fix

--- templates/bootstrap/loop/select-sort.php
+++ templates/bootstrap/loop/select-sort.php
@@ -1,49 +1,59 @@
 <?php
 /**
- * Select Sort
+ * Select Sort Options
  *
- * @ver 1.0.0
+ * This template can be overridden by copying it to yourtheme/geodirectory/bootstrap/loop/select-sort.php.
+ *
+ * HOWEVER, on occasion GeoDirectory will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see        https://wpgeodirectory.com/documentation/article/how-tos/customizing-templates/
+ * @package    GeoDirectory
+ * @version    2.8.119
+ *
+ * @param array  $sort_options Sort options.
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
 global $aui_bs5;
 
-// get the items first so we can label the button with current sort option
-$button_label = esc_attr__( 'Sort By', 'geodirectory' );
+// Get the items first so we can label the button with current sort option
+$button_label = __( 'Sort By', 'geodirectory' );
 $sort_options_html = '';
-if(!empty($sort_options )) {
+
+if ( ! empty( $sort_options ) ) {
 	foreach ( $sort_options as $sort ) {
 		$value = '';
+
 		if ( $sort->field_type == 'random' ) {
 			$value = "random";
-		} elseif ( $sort->sort == 'asc' ) {
-			$value = esc_attr( $sort->htmlvar_name . "_asc" );
-		} elseif ( $sort->sort == 'desc' ) {
-			$value = esc_attr( $sort->htmlvar_name . "_desc" );
+		} else if ( $sort->sort == 'asc' ) {
+			$value = $sort->htmlvar_name . "_asc";
+		} else if ( $sort->sort == 'desc' ) {
+			$value = $sort->htmlvar_name . "_desc";
 		}
-		$active = ( $value && ! empty( $_REQUEST['sort_by'] ) && esc_attr( $_REQUEST['sort_by'] ) == $value ) || ( $sort->is_default == '1' && ! isset( $_REQUEST['sort_by'] ) ) ? 'active' : '';
 
-		if($active){
-			$button_label = esc_attr($sort->frontend_title );
+		$active = ( $value && ! empty( $_REQUEST['sort_by'] ) && $_REQUEST['sort_by'] == $value ) || ( $sort->is_default == '1' && ! isset( $_REQUEST['sort_by'] ) ) ? ' active' : '';
+
+		if ( $active ) {
+			$button_label = $sort->frontend_title;
 		}
-		$sort_options_html .= '<a href="'.esc_url( add_query_arg( 'sort_by', $value ) ).'" class="dropdown-item '.$active.'">'.esc_attr($sort->frontend_title ).'</a>';
+
+		$sort_options_html .= '<a href="' . esc_url( add_query_arg( 'sort_by', $value ) ) . '" class="dropdown-item' . esc_attr( $active ) . '" rel="nofollow">' . esc_html( $sort->frontend_title ) . '</a>';
 	}
 }
 ?>
-
-<div class="btn-group btn-group-sm geodir-sort-by" role="group" aria-label="<?php esc_attr_e("Sort by","geodirectory");?>">
-	<div class="btn-group btn-group-sm" role="group">
-		<button id="geodir-sort-by" type="button" class="btn btn-outline-primary <?php echo $aui_bs5 ? 'rounded-end dropdown-toggle dropdown-toggle-0' : 'rounded-right'; ?>" data-<?php echo ( $aui_bs5 ? 'bs-' : '' ); ?>toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			<?php echo $button_label;?> <i class="fas fa-sort"></i>
-		</button>
-		<div class="dropdown-menu dropdown-caret-0 my-3 p-0" aria-labelledby="gd-list-view-select-grid">
-			<h6 class="dropdown-header"><?php esc_attr_e("Sort Options","geodirectory");?></h6>
-			<?php echo $sort_options_html;?>
-			<div class="dropdown-divider"></div>
-			<a class="dropdown-item" href="<?php echo esc_url( remove_query_arg( 'sort_by' ) );?>"><?php esc_attr_e("Default","geodirectory");?></a>
-		</div>
+<div class="btn-group btn-group-sm geodir-sort-by" role="group" aria-label="<?php esc_attr_e( "Sort by", "geodirectory" ); ?>">
+	<button id="geodir-sort-by" type="button" class="btn btn-outline-primary <?php echo $aui_bs5 ? 'rounded-end dropdown-toggle dropdown-toggle-0' : 'rounded-right'; ?>" data-<?php echo ( $aui_bs5 ? 'bs-' : '' ); ?>toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo esc_html( $button_label ); ?> <i class="fas fa-sort"></i>
+	</button>
+	<div class="dropdown-menu dropdown-caret-0 my-3 p-0" aria-labelledby="gd-list-view-select-grid">
+		<h6 class="dropdown-header"><?php echo esc_html__( "Sort Options", "geodirectory" ); ?></h6>
+		<?php echo $sort_options_html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+		<div class="dropdown-divider"></div>
+		<a class="dropdown-item" href="<?php echo esc_url( remove_query_arg( 'sort_by' ) );?>" rel="nofollow"><?php echo esc_html__( "Default", "geodirectory" ); ?></a>
 	</div>
 </div>
--- templates/loop/select-sort.php
+++ templates/loop/select-sort.php
@@ -1,18 +1,24 @@
 <?php
 /**
- * Select Sort
+ * Select Sort Options
  *
- * @ver 2.8.119
+ * This template can be overridden by copying it to yourtheme/geodirectory/loop/select-sort.php.
+ *
+ * HOWEVER, on occasion GeoDirectory will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see        https://wpgeodirectory.com/documentation/article/how-tos/customizing-templates/
+ * @package    GeoDirectory
+ * @version    2.8.119
+ *
+ * @param array  $sort_options Sort options.
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
-global $aui_bs5;
-
-// Get the items first so we can label the button with current sort option
-$button_label = __( 'Sort By', 'geodirectory' );
 $sort_options_html = '';
 
 if ( ! empty( $sort_options ) ) {
@@ -27,24 +33,15 @@ if ( ! empty( $sort_options ) ) {
 			$value = $sort->htmlvar_name . "_desc";
 		}
 
-		$active = ( $value && ! empty( $_REQUEST['sort_by'] ) && $_REQUEST['sort_by'] == $value ) || ( $sort->is_default == '1' && ! isset( $_REQUEST['sort_by'] ) ) ? 'active' : '';
+		$selected = ( $value && ! empty( $_REQUEST['sort_by'] ) && $_REQUEST['sort_by'] == $value ) || ( $sort->is_default == '1' && ! isset( $_REQUEST['sort_by'] ) ) ? true : false;
 
-		if ( $active ) {
-			$button_label = $sort->frontend_title;
-		}
-
-		$sort_options_html .= '<a href="' . esc_url( add_query_arg( 'sort_by', $value ) ) . '" class="dropdown-item ' . esc_attr( $active ) . '" rel="nofollow">' . esc_html( $sort->frontend_title ) . '</a>';
+		$sort_options_html .= '<option value="' . esc_url( add_query_arg( 'sort_by', $value ) ) . '" ' . selected( $selected, true, false ) . '>' . esc_html( $sort->frontend_title ) . '</option>';
 	}
 }
 ?>
-<div class="btn-group btn-group-sm geodir-sort-by" role="group" aria-label="<?php esc_attr_e( "Sort by", "geodirectory" ); ?>">
-	<div class="btn-group btn-group-sm" role="group">
-		<button id="geodir-sort-by" type="button" class="btn btn-outline-primary <?php echo $aui_bs5 ? 'rounded-end dropdown-toggle dropdown-toggle-0' : 'rounded-right'; ?>" data-<?php echo ( $aui_bs5 ? 'bs-' : '' ); ?>toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo esc_html( $button_label ); ?> <i class="fas fa-sort"></i></button>
-		<div class="dropdown-menu dropdown-caret-0 my-3 p-0" aria-labelledby="gd-list-view-select-grid">
-			<h6 class="dropdown-header"><?php echo esc_html__( "Sort Options", "geodirectory" ); ?></h6>
-			<?php echo $sort_options_html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-			<div class="dropdown-divider"></div>
-			<a class="dropdown-item" href="<?php echo esc_url( remove_query_arg( 'sort_by' ) );?>" rel="nofollow"><?php echo esc_html__( "Default", "geodirectory" ); ?></a>
-		</div>
-	</div>
+<div class="geodir-tax-sort">
+	<select name="sort_by" class="geodir-select geodir-sort-by" aria-label="<?php esc_attr_e( 'Sort By' ,'geodirectory' ); ?>">>
+		<option value="<?php echo esc_url( add_query_arg( 'sort_by', '' ) ); ?>"><?php esc_html_e( 'Sort By', 'geodirectory' ); ?></option>
+		<?php echo $sort_options_html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+	</select>
 </div>
